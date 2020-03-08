48d36682bd3e7f48b1b1f35dbfbe1211d7ce24a2
Usuario <agentcarvajal@gmail.com>
2020-03-07 22:30:28 -0500

    frontend sergio

--- file/filtros.py
+++ file/filtros.py
@@ -6,23 +6,11 @@ from .models import Estado, RegistroLlamada
 
 
 class filtro(django_filters.FilterSet):
-    id_estado__nombre = django_filters.ModelMultipleChoiceFilter(
-        queryset=Estado.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-control search-slt',
-                                           'id': 'exampleFormControlSelect1',
-
-                                           }))
-
-    id_usuario__telefono_fijo = django_filters.ModelMultipleChoiceFilter(
-        queryset=Perfil.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-control search-slt',
-                                   'id': 'exampleFormControlSelect1'}))
-
-    id_usuario__fechaEntrega = django_filters.ModelMultipleChoiceFilter(
-        queryset=RegistroLlamada.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-control search-slt',
-                                   'id': 'exampleFormControlSelect1'}))
+    id_estado = django_filters.ModelMultipleChoiceFilter(queryset=Estado.objects.all(),
+                                                         widget=forms.CheckboxSelectMultiple)
+    id_usuario = django_filters.ModelMultipleChoiceFilter(queryset=Perfil.objects.all(),
+                                                          widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = RegistroLlamada
-        fields = ['id_estado']
+        fields = ['id_estado', 'id_usuario']
