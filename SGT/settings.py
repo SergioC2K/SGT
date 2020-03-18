194bfae5e681cf0312d2ea06283d40210fc5c0fe
Andres <andresuchimaserna@gmail.com>
2020-03-18 16:16:55 -0500

    commit salvada 4:16Pm 18/03/2020

--- SGT/settings.py
+++ SGT/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'SGT.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sgt',
+        'NAME': 'sgt1',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
